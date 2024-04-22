3951c7336b9a281f7e17acb3c547a9b377b5b80c
Tengue Moïse <50266948+tkmmoise@users.noreply.github.com>
2024-04-22 11:51:58 +0200

    add tests

---
+++ jest.config.cjs
@@ -0,0 +1,15 @@
+// list to add ESM to ignore
+const esModules = ['uuid'].join('|');
+
+
+module.exports = {
+  testEnvironment: "node",
+  reporters: [
+    "default", // Utilise le reporter standard
+    ["jest-junit", {
+      outputDirectory: "test_results", // Dossier où les résultats seront enregistrés
+      outputName: "junit.xml" // Nom du fichier de sortie
+    }]
+  ]
+};
+
---
+++ tests/api.test.js
@@ -0,0 +1,37 @@
+const request = require('supertest');
+const app = require('../backend/server');
+let server;
+
+beforeAll((done) => {
+    server = app.listen(5000, () => {
+        console.log('Test server running on port 5001');
+        done();
+    });
+});
+
+afterAll((done) => {
+    server.close(() => {
+        console.log('Test server closed');
+        done();
+    });
+});
+
+describe('API Tests', () => {
+    it('GET /contacts - should return all contacts', async () => {
+        const response = await request(server).get('/contacts');
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toBeInstanceOf(Array);
+    });
+
+    it('POST /contacts - should create a new contact', async () => {
+        const newContact = {
+            name: 'John Doe',
+            email: 'john@example.com',
+            phone: '1234567890'
+        };
+        
+        const response = await request(server).post('/contacts').send(newContact);
+        expect(response.statusCode).toBe(201);
+        expect(response.body).toMatchObject(newContact);
+    });
+});
